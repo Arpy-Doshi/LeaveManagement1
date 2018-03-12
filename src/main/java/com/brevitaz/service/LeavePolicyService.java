3e215901c8064c1005e219d19ae5518d2c5e258f
yashpatel <yashpatel2236@gmail.com>
2018-03-12 15:00:31 +0530

    .

--- src/main/java/com/brevitaz/service/LeavePolicyService.java
+++ src/main/java/com/brevitaz/service/LeavePolicyService.java
@@ -1,4 +1,14 @@
 package com.brevitaz.service;
 
+import com.brevitaz.model.LeavePolicy;
+
+import java.util.List;
+
 public interface LeavePolicyService {
+
+    public boolean create(LeavePolicy leavePolicy);
+    public boolean update(LeavePolicy leavePolicy,String id);
+    public boolean delete( String id);
+    public LeavePolicy getById(String id);
+    public List<LeavePolicy> getAll();
 }
--- src/main/java/com/brevitaz/service/impl/LeavePolicyServiceImpl.java
+++ src/main/java/com/brevitaz/service/impl/LeavePolicyServiceImpl.java
@@ -1,4 +1,34 @@
 package com.brevitaz.service.impl;
 
-public class LeavePolicyServiceImpl {
+import com.brevitaz.dao.LeavePolicyDao;
+import com.brevitaz.model.LeavePolicy;
+import com.brevitaz.service.LeavePolicyService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class LeavePolicyServiceImpl implements LeavePolicyService {
+
+    @Autowired
+    private LeavePolicyDao leavePolicyDao;
+
+    public boolean create(LeavePolicy leavePolicy){
+        return leavePolicyDao.create(leavePolicy);
+    }
+
+    public boolean update(LeavePolicy leavePolicy,String id){
+        return leavePolicyDao.update(leavePolicy,id);
+    }
+    public boolean delete(String id) {
+        return leavePolicyDao.delete(id);
+    }
+
+    public LeavePolicy getById(String id) {
+        return leavePolicyDao.getById(id);
+    }
+
+    public List<LeavePolicy> getAll()  {
+        return leavePolicyDao.getAll();
+    }
+
 }
