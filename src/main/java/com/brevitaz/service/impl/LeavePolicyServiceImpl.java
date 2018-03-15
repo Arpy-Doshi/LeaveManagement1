3eaa045da8d4f120486cadc980808348c4478975
arpy <arpydoshi28@gmail.com>
2018-03-15 13:00:08 +0530

    ..

--- src/main/java/com/brevitaz/service/impl/LeavePolicyServiceImpl.java
+++ src/main/java/com/brevitaz/service/impl/LeavePolicyServiceImpl.java
@@ -4,6 +4,8 @@ import com.brevitaz.dao.LeavePolicyDao;
 import com.brevitaz.model.LeavePolicy;
 import com.brevitaz.service.LeavePolicyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,21 +18,52 @@ public class LeavePolicyServiceImpl implements LeavePolicyService
     private LeavePolicyDao leavePolicyDao;
 
     @Override
-    public boolean create(LeavePolicy leavePolicy) {
+    public boolean create(LeavePolicy leavePolicy)
+    {
+        if (leavePolicy.getId().trim().length()<=0 || leavePolicy.getName().trim().length()<=0)
+            throw new RuntimeException("Field is Null");
+         else if (leavePolicy.getLeavePolicyRules().isEmpty())
+             throw new RuntimeException("Rules are empty!!");
+         else if (leavePolicy != null)
             return leavePolicyDao.create(leavePolicy);
+        else
+            throw new RuntimeException("Bad Request!!!");
     }
 
     @Override
     public boolean update(LeavePolicy leavePolicy, String id)
     {
-      return leavePolicyDao.update(leavePolicy,id);
+        LeavePolicy leavePolicy1 = leavePolicyDao.getById(leavePolicy.getId());
+
+        if (id.trim().length()<=0)
+            throw new RuntimeException("Id is null!!");
+        else if (leavePolicy.getId().trim().length()<=0 || leavePolicy.getName().trim().length()<=0)
+            throw new RuntimeException("Field is null!!!");
+        else if (leavePolicy1.getId() != id)
+            throw new RuntimeException("Id doesn't match");
+        else if(leavePolicy!=null && leavePolicy.getId().equals(id))
+            return leavePolicyDao.update(leavePolicy,id);
+       else
+            throw new RuntimeException("Bad Request!!!!");
+
 
     }
 
     @Override
     public boolean delete(String id)
     {
-       return leavePolicyDao.delete(id);
+        LeavePolicy leavePolicy = leavePolicyDao.getById(id);
+
+        if (id.trim().length()<=0)
+            throw new RuntimeException("Id is null!!");
+        else if (leavePolicy.getId().trim().length()<= 0 || leavePolicy.getName().trim().length()<=0)
+            throw new RuntimeException("Field is null");
+        else if (leavePolicy.getId() != id)
+            throw new RuntimeException("Id doesn't match");
+        else if (leavePolicy!= null && leavePolicy.getId().equals(id))
+            return leavePolicyDao.delete(id);
+        else
+            throw new RuntimeException("Bad Request!!");
     }
 
     @Override
